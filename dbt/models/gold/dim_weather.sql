f14ecadcca38b3188e27e239c20da09523e9c6af
Kelli Gagarina <kelli.gagarina23@gmail.com>
2025-10-28 17:54:19 +0200

    fix silver and golden layer

--- dbt/models/gold/dim_weather.sql
+++ dbt/models/gold/dim_weather.sql
@@ -1,10 +1,15 @@
 {{ config(materialized='table') }}
 
 SELECT
-  toUInt32(toYYYYMMDD(date)) AS weather_id,
-  date,
-  temperature_max,
-  temperature_min,
-  rain_sum,
-  precipitation_hours
+    toUInt32(toYYYYMMDD(date)) AS weather_id,
+    date,
+    weather_code,
+    temperature_mean,
+    temperature_max,
+    temperature_min,
+    rain_sum,
+    snowfall_sum,
+    precipitation_hours,
+    wind_speed_10m_max,
+    shortwave_radiation_sum
 FROM {{ ref('silver_weather') }}
--- dbt/models/silver/silver_weather.sql
+++ dbt/models/silver/silver_weather.sql
@@ -1,26 +1,41 @@
-{{ config(materialized='incremental', unique_key='date') }}
+{{ config(
+    materialized='incremental',
+    unique_key='date'
+) }}
 
 WITH base AS (
-  SELECT
-    date,
-    temperature_max,
-    temperature_min,
-    rain_sum,
-    precipitation_hours
-  FROM bronze.weather_raw
+    SELECT
+        date,
+        temperature_2m_max, 
+        temperature_2m_min,
+        temperature_2m_mean ,
+        rain_sum,
+        snowfall_sum,
+        precipitation_hours,
+        wind_speed_10m_max,
+        shortwave_radiation_sum,
+        weather_code
+    FROM bronze.weather_raw
 ),
+
 daily AS (
   SELECT
     date,
     any(temperature_max) AS temperature_max,
     any(temperature_min) AS temperature_min,
+    any(temperature_2m_mean) AS temperature_mean,
     any(rain_sum)        AS rain_sum,
-    any(precipitation_hours) AS precipitation_hours
+    any(snowfall_sum)    AS snowfall_sum,
+    any(precipitation_hours) AS precipitation_hours,
+    any(wind_speed_10m_max) AS wind_speed_10m_max,
+    any(shortwave_radiation_sum) AS shortwave_radiation_sum,
+    any(weather_code) AS weather_code
   FROM base
   GROUP BY date
 )
 SELECT *
 FROM daily
 {% if is_incremental() %}
-WHERE date > (SELECT coalesce(max(date), toDate('1900-01-01')) FROM {{ this }})
+WHERE date > (SELECT coalesce(max(date), toDateTime('1900-01-01 00:00:00')) FROM {{ this }})
 {% endif %}
+
